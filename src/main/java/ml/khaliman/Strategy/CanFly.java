85c8fb40d24869b7d783960ad4af667809289389
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-10-18 23:57:57 +0300

    delete starategy

--- src/main/java/ml/khaliman/Strategy/CanFly.java
+++
@@ -1,8 +0,0 @@
-package Patterns.Strategy;
-
-public class CanFly implements Strategy {
-    @Override
-    public void algorithm() {
-        System.out.println("Эльфы и пегасы умеют только летать");
-    }
-}
--- src/main/java/ml/khaliman/Strategy/CanFly_Walk.java
+++
@@ -1,8 +0,0 @@
-package Patterns.Strategy;
-
-public class CanFly_Walk implements Strategy {
-    @Override
-    public void algorithm() {
-        System.out.println("Летать и ходить умеют только тролли и вампиры");
-    }
-}
