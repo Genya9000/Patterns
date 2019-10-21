55c389647cce2d01c77f7b7f0c85593e15ff5fa6
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-10-21 22:21:22 +0300

    bilder

---
+++ src/main/java/ml/khaliman/Bilder/BuilderExample.java
@@ -0,0 +1,12 @@
+package ml.khaliman.Bilder;
+class BuilderExample {
+    public static void main(String[] args) {
+        Director director = new Director();
+        ComputerBuilder cheapComputerBuilder = new CheapComputerBuilder();
+
+        director.setComputerBuilder(cheapComputerBuilder);
+        director.constructComputer();
+
+        Computer computer = director.getComputer();
+    }
+}
--- src/main/java/ml/khaliman/Factory/Second.java
+++
@@ -1,8 +0,0 @@
-package Patterns.Factory;
-
-public class Second implements Figure {
-    @Override
-    public void getFigure() {
-        System.out.println("Я фигура с двумя квадратами");
-    }
-}
