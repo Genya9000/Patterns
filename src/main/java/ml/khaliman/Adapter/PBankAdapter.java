62cd27b13cd3aa9ef66981d4130170627775bdd0
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-11-10 22:52:01 +0200

    add adapter

---
+++ src/main/java/ml/khaliman/Adapter/PBankAdapter.java
@@ -0,0 +1,9 @@
+class PBankAdapter extends PBank {
+    private ABank abank;
+    public PBankAdapter(ABank abank) {
+        this.abank = abank;
+    }
+    public void getBalance() {
+        abank.getBalance();
+    }
+}
