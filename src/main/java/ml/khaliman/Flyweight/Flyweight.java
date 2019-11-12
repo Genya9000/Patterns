b184ca1a64c6f0ea35be69769f20f7fee25f4e97
Yevhen Khaliman <Eugen.Haliman@gmail.com>
2019-11-12 23:26:16 +0200

    add flyweight

---
+++ src/main/java/ml/khaliman/Flyweight/Flyweight.java
@@ -0,0 +1,11 @@
+package ml.khaliman.Flyweight;
+class Flyweight {
+    private int row;
+    public Flyweight(int row) {
+        this.row = row;
+        System.out.println("ctor: " + this.row);
+    }
+    void report(int col) {
+        System.out.print(" " + row + col);
+    }
+}
